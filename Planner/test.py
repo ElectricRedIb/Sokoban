31cb4882bce900387f8277182de7680fb48c45f6
patrickfp <papet15@student.sdu.dk>
2018-12-02 13:53:51 +0100

    changes to glue, and map file updated

--- Planner/test.py
+++ Planner/test.py
@@ -30,6 +30,7 @@ temp = False
 direction = "L"
 
 def makeMobe(string,pos,direction):
+    global glueSolution
     global pushingCan
     state = string
     if direction == 'L':
@@ -46,75 +47,69 @@ def makeMobe(string,pos,direction):
         if direction == 'D':
             if state[x + cols] == '.' or state[x + cols] == 'G':
                 string = state[:pos] + '.' + state[pos + 1:x] + 'M' + state[x + 1:x + cols] + 'J' + state[x + cols + 1:]
-                pushingCan = True
         elif direction == 'R':
             if state[x + 1] == '.' or state[x + 1] == 'G':
                 string = state[:x - 1] + '.MJ' + state[x + 2:]
-                pushingCan = True
         elif direction == 'U':
             if state[x - cols] == '.' or state[x - cols] == 'G':
                 string = state[:x - cols] + "J" + state[x - cols + 1:x] + 'M' + state[x + 1:pos] + '.' + state[pos + 1:]
-                pushingCan = True
         elif direction == 'L':
             if state[x - 1] == '.' or state[x - 1] == 'G':
                 string = state[:x - 1] + 'JM.' + state[x + 2:]
-                pushingCan = True
+        pushingCan = True
     elif state[x] == '.' or state[x] == 'G':
+        if pushingCan:
+            glueSolution = glueSolution + 'D'
         if direction == 'D':
             string = state[:pos] + '.' + state[pos + 1:x] + 'M' + state[x + 1:]
-            pushingCan = False
         elif direction == 'R':
             string = state[:pos] + '.M' + state[x + 1:]
-            pushingCan = False
         elif direction == 'U':
             string = state[:x] + 'M' + state[x + 1:pos] + '.' + state[pos + 1:]
-            pushingCan = False
         elif direction == 'L':
             string = state[:x] + 'M.' + state[pos + 1:]
-            pushingCan = False
+        pushingCan = False
     return string
         # self.TreeOfStates.append(node.makeChild(string,tempPos[idx]))
 
+def print_map(string):
+    printstring = "\n"
+    i = 0
+    for c in string:
+        printstring += c
+        printstring += "\t"
+        i += 1
+        if i > cols - 1:
+            i = 0
+            printstring += "\n"
+    printstring += "\n"
+    print(printstring)
+
 
 for idx, c in enumerate(solution):
+    #print_map(state)
     if direction == c:
-        glueSolution = glueSolution + "F"
         state = makeMobe(state,pos,direction)
-        if temp == True and pushingCan == False:
-            glueSolution = glueSolution + 'D'
-            temp = pushingCan
-        else:
-            temp = pushingCan
-    elif direction == "L" and c == 'U' or direction == "U" and c == 'R' or  direction == "R" and c == 'D' or  direction == "D" and c == 'L': #Turn Right
-        direction = c
-        state = makeMobe(state, pos, direction)
-        if temp == True and pushingCan == False:
-            glueSolution = glueSolution + 'D'
-            temp = pushingCan
-        else:
-            temp = pushingCan
-        glueSolution = glueSolution + 'R'
         glueSolution = glueSolution + "F"
-    elif direction == "L" and c == 'D' or direction == "U" and c == 'L' or  direction == "R" and c == 'U' or  direction == "D" and c == 'R': #Turn Left
-        direction = c
-        state = makeMobe(state, pos, direction)
-        if temp == True and pushingCan == False:
+    else:
+        if pushingCan:
             glueSolution = glueSolution + 'D'
-            temp = pushingCan
-        else:
-            temp = pushingCan
-        glueSolution = glueSolution + 'L'
-        glueSolution = glueSolution + "F"
-    elif direction == "L" and c == 'R' or direction == "R" and c == 'L' or  direction == "U" and c == 'D' or  direction == "D" and c == 'U': #Turn Back
-        direction = c
-        state = makeMobe(state, pos, direction)
-        if temp == True and pushingCan == False:
-            glueSolution = glueSolution + 'D'
-            temp = pushingCan
-        else:
-            temp = pushingCan
-        glueSolution = glueSolution + 'RR'
-        glueSolution = glueSolution + "F"
+            pushingCan = False
+        if direction == "L" and c == 'U' or direction == "U" and c == 'R' or  direction == "R" and c == 'D' or  direction == "D" and c == 'L': #Turn Right
+            direction = c
+            state = makeMobe(state, pos, direction)
+            glueSolution = glueSolution + 'R'
+            glueSolution = glueSolution + "F"
+        elif direction == "L" and c == 'D' or direction == "U" and c == 'L' or  direction == "R" and c == 'U' or  direction == "D" and c == 'R': #Turn Left
+            direction = c
+            state = makeMobe(state, pos, direction)
+            glueSolution = glueSolution + 'L'
+            glueSolution = glueSolution + "F"
+        elif direction == "L" and c == 'R' or direction == "R" and c == 'L' or  direction == "U" and c == 'D' or  direction == "D" and c == 'U': #Turn Back
+            direction = c
+            state = makeMobe(state, pos, direction)
+            glueSolution = glueSolution + 'RR'
+            glueSolution = glueSolution + "F"
     if direction == 'L':
         pos = pos - 1
     elif direction == 'R':
